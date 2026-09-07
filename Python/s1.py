a56fb09d60d626918e74c1237ebd2fc739791791
Yuvraj Soni <yuvrajsoni9192@gmail.com>
2026-09-07 19:22:56 +0530

    07-09-2026

---
+++ Python/s1.py
@@ -0,0 +1,8 @@
+s = "Hello World"
+print(id(s))
+print(f"The word loop : {s[0:7:2]}" )
+
+lable_text = "Chai Special"
+ecoded_text = lable_text.encode("utf-8")
+print(ecoded_text)
+print(lable_text)
