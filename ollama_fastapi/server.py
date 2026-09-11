7778b57d6ef4d98ce38eb41cce73774fa9910bb7
Yuvraj Soni <yuvrajsoni9192@gmail.com>
2026-09-11 19:13:39 +0530

    11-09-2026

---
+++ ollama_fastapi/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI,Body
+from ollama import Client
+
+
+app = FastAPI()
+client = Client(
+    host="http://localhost:11434",
+)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/chat" )
+def chat(message: str = Body(...,description="the Message")):
+    response = client.chat(model="llama3.2:3b",messages=[
+        {"role":"user","content":message}
+    ])
+
+    return {"response": response.message.content}
