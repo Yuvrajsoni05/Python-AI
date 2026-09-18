e4d834cde99320de5f22ad37c3fd865355fe8cca
Yuvraj Soni <yuvrajsoni9192@gmail.com>
2026-09-18 18:24:02 +0530

    18-09-2026

--- rag/chat.py
+++ rag/chat.py
@@ -24,6 +24,7 @@ user_query =  input("Enter a query: ")
 
 # Relevant Search
 search_result = vector_db.similarity_search(query=user_query)
+print(search_result)
 
 context = "\n\n".join(
     f"# Page Content: {result.page_content}\n"
@@ -52,5 +53,7 @@ response = open_ai.chat.completions.create(
         {"role":"user","content":user_query},
     ]
 )
+print(response)
+# ChatCompletion(id='chatcmpl-EPQvZ6flHgzRw0tzWEh52zsjGSixo', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='A for loop in Python is used to iterate through a sequence like a list, tuple, or string (iterables). The syntax for a for loop is shown below:\n\n```python\nl = [1, 7, 8] \nfor item in l: \n    print(item) # prints 1, 7 and 8\n```\n\n(Page 27 of the PDF)', refusal=None, role='assistant', annotations=[], audio=None, function_call=None, tool_calls=None))], created=1789730417, model='gpt-4o-2024-08-06', object='chat.completion', metadata=None, moderation=None, service_tier='default', system_fingerprint='fp_d77fe65d9d', usage=CompletionUsage(completion_tokens=79, prompt_tokens=989, total_tokens=1068, completion_tokens_details=CompletionTokensDetails(accepted_prediction_tokens=0, audio_tokens=0, reasoning_tokens=0, rejected_prediction_tokens=0, text_tokens=None), prompt_tokens_details=PromptTokensDetails(audio_tokens=0, cache_write_tokens=None, cached_tokens=0, image_tokens=None, text_tokens=None)))
 
 print(f"Response: {response.choices[0].message.content}")
